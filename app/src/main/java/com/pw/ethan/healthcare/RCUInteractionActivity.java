799671d09d30e3996ee4587441b2c3f69a5ab00a
pw-ethan <weilaiwen92@126.com>
2017-07-20 15:02:00 +0800

    RCUInteractionActivity-now is ok

--- app/src/main/java/com/pw/ethan/healthcare/RCUInteractionActivity.java
+++ app/src/main/java/com/pw/ethan/healthcare/RCUInteractionActivity.java
@@ -34,7 +34,7 @@ public class RCUInteractionActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cloudsersendactivity);
+        setContentView(R.layout.activity_rcuinteraction);
 
         findViewById(R.id.send).setOnClickListener(listener);
         findViewById(R.id.hash).setOnClickListener(listener);
